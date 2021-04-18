9461910a606ad4b929d87fbdbb309e19e9386ed1
Mourad Bougarne <contact@mbougarne.me>
2021-04-18 14:36:25 +0000

    Clear src/index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
@@ -10,13 +10,5 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
-
-app.get('/', (req: Request, res: Response) => {
-	res.status(200).json({
-		success: true,
-		status: 200,
-		path: req.url,
-	});
-});
 
 export default app;
